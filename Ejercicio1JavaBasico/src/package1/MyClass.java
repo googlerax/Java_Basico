394ef7d099ca2f62d2b5f97e50c3610e8bb8edd4
googlerax <46288252+googlerax@users.noreply.github.com>
2022-02-14 19:38:17 -0300

    Add files via upload
    Ejercicio 1 de java basico.

---
+++ Ejercicio1JavaBasico/src/package1/MyClass.java
@@ -0,0 +1,28 @@
+package package1;
+
+public class MyClass {
+
+
+    public static void main(String[] args) {
+
+        // Tipos de datos:
+
+        byte num = 2;
+        int num1 = 10;
+        long num2 = 10000;
+        float num3 = 15.2f;
+        double num4 = 15.45d;
+        boolean open = true;
+        boolean close = false;
+        char caracteres = 'z';
+        String texto = "Esto es una cadena de texto";
+
+
+        System.out.println("tipos de datos numericos : " + num + " / " + num1 + " / " + num2 + " / " + num3 + " / " + num4);
+        System.out.println("tipos de datos condicionales : " + open + " y " + close);
+        System.out.println("representa caracteres : " + caracteres);
+        System.out.println(texto);
+
+
+    }
+}
